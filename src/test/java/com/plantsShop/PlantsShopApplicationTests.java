f08fd9eadf33d460505f2491c46ba7eb1f395340
kenca <kenca@192.168.0.44>
2024-02-05 09:29:41 -0600

    Practica 2 Desarrollo de Aplicaciones Web y Patrones

---
+++ src/test/java/com/plantsShop/PlantsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantsShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
